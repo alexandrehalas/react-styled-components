0e82f2ab442f5acdb6b941ce7c35cbce12746174
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-29 21:02:22 -0300

    Aplicado o styled components ao componente Titulo

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import styled from 'styled-components';
 
-const Titulo = ({ children }) => {
-  return <h1 className="titulo">{children}</h1>;
-};
+// sintaxe de template literals
+const Titulo = styled.h1`
+  color: grey;
+  padding: 25px 0;
+`
+
 export default Titulo;
