d7bfc6fbab6917ac54881e11d5bb29dae8f82602
Alexandre Halas <alexandrehalas@gmail.com>
2023-05-01 13:51:41 -0300

    Refatorando para componentes estilizados demais itens do componente Conta e Container

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -15,6 +15,9 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `
 
 export default () => {
