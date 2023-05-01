7e446bff446b7da7f5c6db491a3cb4dd6399fc17
Alexandre Halas <alexandrehalas@gmail.com>
2023-05-01 15:12:59 -0300

    Atribuída a funcionalidade de alterar tema no App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 
+import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
-import { temaEscuro } from "./Components/UI/temas";
+import SwitcherTheme from "./Components/SwitcherTheme";
+import { ThemeButton } from "./Components/UI";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 function App() {
+
+  const [theme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setTheme((theme) => !theme)
+  }
+
   return (
-    <ThemeProvider theme={temaEscuro}>
+    <ThemeProvider theme={theme ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <ThemeButton onClick={toggleTheme}>
+        <SwitcherTheme theme={theme} />
+      </ThemeButton>
       <Cabecalho />
       <Container />
     </ThemeProvider>
