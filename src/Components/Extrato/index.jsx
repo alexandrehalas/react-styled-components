544351016ebd209c15859b3a20b1e3828082b9e4
Alexandre Halas <alexandrehalas@gmail.com>
2023-05-01 14:22:17 -0300

    Construído componentes Itens e Item e aplicado ao componente Extrato

--- src/Components/Extrato/index.jsx
+++ src/Components/Extrato/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { extratoLista } from '../../info'
+import Itens from '../Itens'
 import { Box, Button } from '../UI'
 
 export default function Extrato() {
@@ -7,10 +8,7 @@ export default function Extrato() {
         <Box>
             {extratoLista.updates.map(({ id, type, value, date, from }) => {
                 return (
-                    <div key={id}>
-                        <div>{type}</div>
-                        <div>{from}</div>
-                    </div>
+                    <Itens key={id} type={type} value={value} date={date} from={from} />
                 )
             })}
             <Button>Ver mais</Button>
